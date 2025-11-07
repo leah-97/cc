cd3c9d76fa4219d206c6775ca79dfd65abaeff00
hazel0209 <karisk0209@gmail.com>
2025-11-07 12:27:14 +0900

    alldev

--- Bulk purchase/js/custom.js
+++ Bulk purchase/js/custom.js
@@ -1,3 +1,25 @@
+$(function () {
+  $(window).on("scroll", function () {
+    let space = $(window).scrollTop();
+    if (space > 10) {
+      $("header").addClass("on");
+      $("header .head_w").removeClass("on");
+      $("header .head_r").addClass("on");
+    } else {
+      $("header .head_r").removeClass("on");
+      $("header .head_w").addClass("on");
+      $("header").removeClass("on");
+    }
+  });
+
+  $("header .bars").on("click", function () {
+    $("#menu").animate({ right: "-50px" }, 600, "swing");
+  });
+  $("#menu .hclose").on("click", function () {
+    $("#menu").animate({ right: "-100%" }, 600, "swing");
+  });
+});
+
 document.addEventListener("DOMContentLoaded", () => {
   // ======================
   // 날짜 선택기 (flatpickr)
